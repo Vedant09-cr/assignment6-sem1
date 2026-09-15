909bf01692294fb4f2dcab8cd1d25b5fb6b187b3
Vedant09-cr <vedantsomani09@gmail.com>
2026-09-15 10:49:17 +0530

    Initial commit

---
+++ q2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int a = 0, b = 1, c;
+    int n, i;
+
+    printf("enter a number: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++){
+        printf("%d ", a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
+ return 0;
+}
