01ae9c343d0ba8ac76c1a7283141b6dc55c37247
Vedant09-cr <vedantsomani09@gmail.com>
2026-09-17 00:25:34 +0530

    q7

---
+++ q7.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, r, b = 0, p = 1;
+
+    printf("Enter a positive decimal number: ");
+    scanf("%d", &n);
+
+    while(n > 0)
+    {
+        r = n % 2;
+        b = b + r * p;
+        p = p * 10;
+        n = n / 2;
+    }
+
+    printf("Binary number = %d\n", b);
+
+    return 0;
+}
